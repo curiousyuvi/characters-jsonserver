6547a588da034e803da650bc7af984978f7ade64
Yuvraj Singh <ysgaur9919@gmail.com>
2023-03-01 15:57:13 +0530

    fixes some router name

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("json_server/weeklyFeature.json");
+const router = jsonServer.router("json_server/characters.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 5000;
 
